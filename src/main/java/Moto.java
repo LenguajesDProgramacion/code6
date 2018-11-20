c81f294b07492b67633354b1d16cb84942898000
Alex Roman <alextorrez448@gmail.com>
2018-11-19 20:06:26 -0400

    ejercicioImplements

---
+++ src/main/java/Moto.java
@@ -0,0 +1,11 @@
+public class Moto implements Ejemplo{
+
+    public void moverRueda(){
+        System.out.println("mover rueda moto");
+    }
+
+    public void estadoRueda(){
+        System.out.println("estado regular de la rueda de moto");
+    }
+
+}
---
+++ src/main/java/Vehivulo.java
@@ -0,0 +1,10 @@
+public class Vehivulo implements Ejemplo {
+
+    public void moverRueda(){
+        System.out.println("Mover rueda de vehiculo");
+    }
+
+    public void estadoRueda(){
+        System.out.println("La rueda del vehiculo se encuentra en perfecto estado");
+    }
+}
